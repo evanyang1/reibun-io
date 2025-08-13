1e679b7a9e0a347813bb31785c8a0a5432435302
Evan Yang <evanjyang4@gmail.com>
2025-08-13 15:59:26 -0700

    work on homepage

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { CiSearch } from "react-icons/ci";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -7,8 +8,27 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div className="">
-      <h1>Reibun.io</h1>
+    <div className="pt-20">
+      <h1 className="text-7xl text-center font-bold text-blue-600">
+        Reibun.io
+      </h1>
+      <h2 className="text-2xl text-center mt-4">
+        Search for Japanese example sentences!
+      </h2>
+      <section className="flex flex-row items-center mt-10 justify-center">
+        <input
+          type="text"
+          placeholder="i.e. 機会"
+          className="mt-4 block mx-auto p-2 border border-gray-300 rounded"
+        />
+        <button className="bg-blue-200 rounded px-4 py-2 ml-2 text-blue-800 font-semibold hover:bg-blue-300 
+        transition-colors duration-300">
+          <span>
+            <CiSearch />
+          </span>
+          Search
+        </button>
+      </section>
     </div>
   );
 }
