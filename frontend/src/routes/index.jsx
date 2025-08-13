153d5e09165419c7d76d221de35d3e11eb6eb39f
Evan Yang <evanjyang4@gmail.com>
2025-08-13 16:04:38 -0700

    change color scheme

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -9,24 +9,27 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
   return (
     <div className="pt-20">
-      <h1 className="text-7xl text-center font-bold text-blue-600">
+      <h1 className="text-7xl text-center font-bold text-teal-400">
         Reibun.io
       </h1>
       <h2 className="text-2xl text-center mt-4">
         Search for Japanese example sentences!
       </h2>
-      <section className="flex flex-row items-center mt-10 justify-center">
+      <section className="mt-10 flex items-center justify-center gap-2">
         <input
           type="text"
           placeholder="i.e. 機会"
-          className="mt-4 block mx-auto p-2 border border-gray-300 rounded"
+          className="w-72 rounded-md border border-gray-300 p-2.5 shadow-sm focus:border-blue-500
+           focus:ring-teal-500 sm:text-sm"
         />
-        <button className="bg-blue-200 rounded px-4 py-2 ml-2 text-blue-800 font-semibold hover:bg-blue-300 
-        transition-colors duration-300">
-          <span>
-            <CiSearch />
-          </span>
-          Search
+        <button
+          type="button"
+          className="flex items-center justify-center gap-2 rounded-md bg-teal-500 px-4 py-2.5 text-sm font-semibold
+           text-white shadow-sm hover:bg-teal-600 transition-colors focus-visible:outline focus-visible:outline-2 
+           focus-visible:outline-offset-2 focus-visible:outline-blue-600 duration-300"
+        >
+          <CiSearch size={20} />
+          <span>Search</span>
         </button>
       </section>
     </div>
